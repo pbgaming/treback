f578e4162c42d886183a46e166fd4420529896f8
pbgaming <44928010+pbgaming@users.noreply.github.com>
2018-11-23 17:00:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,32 @@ client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
 client.user.setStatus("dnd")
 });
 
-
+client.on('message', message => {
+    var prefix = "_";
+   
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'bc') {
+        if (!args[1]) {
+    message.channel.send("_bc <message>");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                var bc = new Discord.RichEmbed()
+                .addField('» السيرفر :', `${message.guild.name}`)
+                .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+                .addField(' » الرسالة : ', args)
+                .setColor('#ff0000')
+                // m.send(`[${m}]`);
+                m.send(`${m}`,{embed: bc});
+            });
+        }
+        } else {
+            return;
+        }
+    });
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
